08f8d91af30fd99ba85cddbe1831cccfa29fb03c
subinggrae <subin000602@gmail.com>
2024-10-02 14:07:57 +0900

    config(db): mysql2를 이용하여 데이터베이스 커넥션 생성
    host, user, password, database, dateString 속성을 주었음.

---
+++ db.js
@@ -0,0 +1,16 @@
+const mariadb = require('mysql2');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connection = mariadb.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  dateStrings: true
+});
+
+module.exports = {
+  connection
+}
